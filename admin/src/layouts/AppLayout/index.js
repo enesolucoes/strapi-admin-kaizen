f8d02dc598ba0c3cf7c55cdd87601ff866460938
Macbook <macbook@Macbooks-MacBook-Air.local>
2022-10-19 11:43:41 -0300

    Fix: correcao do index do menu para nao ficar por cima das tooltips nem por cima da modal de novos usuarios.

--- admin/src/layouts/AppLayout/index.js
+++ admin/src/layouts/AppLayout/index.js
@@ -19,7 +19,7 @@ const ContainerSideNav = styled.div`
   position: fixed;
   height: 100%;
   background: white;
-  z-index: 9999;
+  z-index: 4;
 `
 
 const AppLayout = ({ children, sideNav, menuCondensed }) => {
